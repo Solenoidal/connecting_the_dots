e75173b512c9a5ef0bfb1129bad11854cc5877df
Solenoid <maruisansmai@gmail.com>
2020-05-06 19:17:16 +0900

    refactor: pages/index.vue -> Button/Sourcecode.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,6 @@
   .flex.flex-col.items-center
     h1.text-6xl CONNECTING THE DOTS
     Logo
-    .btn.p-2.mt-4.text-lg.rounded-md.bg-blue-600.text-white.font-bold
-      a(href="https://github.com/Solenoidal/connecting_the_dots" target="_blank") source code
 </template>
 
 <script>
@@ -22,9 +20,4 @@ export default {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.btn {
-  position: fixed;
-  bottom: 1rem;
-  right: 1rem;
-}
 </style>
