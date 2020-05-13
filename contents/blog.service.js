e54b84a6b60d1d768d2dab552ed608277074989b
Solenoid <maruisansmai@gmail.com>
2020-05-13 10:27:20 +0900

    refactor: functionName, paramName, JSdocs

--- contents/blog.service.js
+++ contents/blog.service.js
@@ -1,22 +1,23 @@
-import blogInfo from '~/contents/summary.json';
+import blogSummary from '~/contents/summary.json';
 
-export const posts = getMetaInfo(blogInfo);
-export const tags = getTags();
+export const posts = getMetaData(blogSummary);
+export const tags = getTags(blogSummary);
 
 /**
  * convert YYYY-MM-DD -> YYYY/MM/DD
  * @param {String} dateString
+ * @return {String} YYYY/MM/DD
  */
 function convertDataString(dateString) {
-  if (!dateString) return false;
-  return dateString.split('-').join('/');
+  return dateString ? dateString.split('-').join('/') : '';
 }
+
 /**
- * get meta data from JSON named summary.json
+ * return meta data from JSON
  * @param {JSON} blogJSON JSON object containing blogs meta data
  * @return {Array} array containing each blog meta data without draft===true
  */
-function getMetaInfo(blogJSON) {
+function getMetaData(blogJSON) {
   const blogDirPatternArray = blogJSON.sourceFileArray.map(
     (str) => str.slice(2, -3) + '.json'
   );
@@ -31,11 +32,12 @@ function getMetaInfo(blogJSON) {
 }
 
 /**
- * @return {Array} - tags extracted from all posts
+ * @param {JSON} blogJSON blogs meta data
+ * @return {Array} tags extracted from all posts
  */
-function getTags() {
+function getTags(blogJSON) {
   const tags = [];
-  const posts = getMetaInfo(blogInfo);
+  const posts = getMetaData(blogJSON);
   posts.forEach((post) => {
     if (post.tags) {
       post.tags.forEach((tag) => tags.push(tag));
