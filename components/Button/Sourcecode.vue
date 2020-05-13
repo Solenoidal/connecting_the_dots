867e452a136ab2ea287fa841131a4539b114d8e3
Solenoid <maruisansmai@gmail.com>
2020-05-13 13:27:12 +0900

    Revert "refactor : change class string"
    This reverts commit 1b995832a0ceba1c4df4975e0d470e129643d2b4.

--- components/Button/Sourcecode.vue
+++ components/Button/Sourcecode.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  div(class="p-2 mt-4 text-lg font-bold text-white bg-blue-600 rounded-md")
+  .p-2.mt-4.text-lg.rounded-md.bg-blue-600.text-white.font-bold
     a(href="https://github.com/Solenoidal/connecting_the_dots" target="_blank") source code
 </template>
 <style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  div(class="flex flex-col items-center")
+  .flex.flex-col.items-center
     h1.text-6xl CONNECTING THE DOTS
     Logo
 </template>
