b279579f54d18bb54ef9dd0bf24982103e335b41
Solenoid <maruisansmai@gmail.com>
2020-05-14 18:16:16 +0900

    add: blog detail page

---
+++ pages/blog/_date/_slug/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <div v-html="bodyHtml"></div>
+    <!-- hello -->
+  </div>
+</template>
+
+<script>
+import { sourceFileArray } from '~/contents/summary.json';
+export default {
+  validate({ params }) {
+    return sourceFileArray.includes(`./contents/blog/${params.slug}/index.md`);
+  },
+  asyncData({ params }) {
+    return Object.assign(
+      {},
+      require(`~/contents/blog/${params.slug}/index.json`),
+      { params }
+    );
+  },
+  head() {
+    return {
+      title: this.title
+    };
+  }
+};
+</script>
+
+<style lang="postcss" scoped></style>
