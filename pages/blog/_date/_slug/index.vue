c61d5a5ed983a2974f19b1d555b1a771bb6ad396
Solenoid <maruisansmai@gmail.com>
2020-05-14 23:39:05 +0900

    adjust: entire style

--- pages/blog/_date/_slug/index.vue
+++ pages/blog/_date/_slug/index.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
-    <h2>{{ title }}</h2>
-    <div v-html="bodyHtml"></div>
-    <!-- hello -->
+  <div class="blog">
+    <h1 class="text-2xl blog__title">{{ title }}</h1>
+    <div class="text-sm text-center blog__date">
+      投稿日/{{ publishedAt }} 更新日/{{ updatedAt }}
+    </div>
+    <div class="blog__body" v-html="bodyHtml"></div>
   </div>
 </template>
 
 <script>
-import { sourceFileArray } from '~/contents/summary.json';
+import { sourceFileArray } from '~/contents/blog.service';
 export default {
   validate({ params }) {
     return sourceFileArray.includes(`./contents/blog/${params.slug}/index.md`);
@@ -27,4 +29,14 @@ export default {
 };
 </script>
 
-<style lang="postcss" scoped></style>
+<style lang="postcss">
+.blog__body > h2 {
+  @apply text-lg border-b border-gray-700 mb-2;
+}
+.blog__body > p {
+  @apply pl-3;
+}
+.blog > :nth-child(n) {
+  @apply mb-2;
+}
+</style>
