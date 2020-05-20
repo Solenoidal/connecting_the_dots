dbe59f0c39700e4e66a74471a39e9f27f5015c39
Solenoid <maruisansmai@gmail.com>
2020-05-20 10:18:11 +0900

    add: more tests

--- components/__test__/Card.spec.js
+++ components/__test__/Card.spec.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-unused-vars */
-import { mount, createLocalVue, Wrapper } from '@vue/test-utils';
-import Card from '~/components/Card';
-import { blogs } from '~/contents/blog.service';
-import { date } from '~/plugins/date';
+import { mount, shallowMount, createLocalVue, Wrapper } from '@vue/test-utils';
+import Card from '~/components/Card.vue';
+import { blogs } from '~/contents/blog.service.js';
+import { date } from '~/plugins/date.js';
 
 const localVue = createLocalVue();
 localVue.prototype.$date = date;
 
-describe('Card', () => {
+describe('Card.vue', () => {
   /** @type {Wrapper<Vue>} */
   let wrapper;
   beforeEach(() => {
@@ -17,7 +17,20 @@ describe('Card', () => {
       stubs: ['nuxt-link']
     });
   });
-  test('is a Vue instance', () => {
-    expect(wrapper.isVueInstance()).toBeTruthy();
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  test('has title and description', () => {
+    expect(wrapper.text()).toMatch(blogs[0].title);
+    expect(wrapper.text()).toMatch(blogs[0].description);
+  });
+
+  test('has a card__header class', () => {
+    expect(wrapper.get('.card__header').exists()).toBe(true);
+  });
+
+  test('has a card class', () => {
+    expect(wrapper.get('.card').exists()).toBe(true);
   });
 });
