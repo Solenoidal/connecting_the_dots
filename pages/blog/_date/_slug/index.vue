dd2656145d293491f764b9ea13434772d199fb6a
Solenoid <maruisansmai@gmail.com>
2020-05-20 10:37:40 +0900

    fix: date

--- pages/blog/_date/_slug/index.vue
+++ pages/blog/_date/_slug/index.vue
@@ -2,14 +2,17 @@
   <div class="blog">
     <h1 class="text-2xl blog__title">{{ title }}</h1>
     <div class="text-sm text-center blog__date">
-      投稿日/{{ publishedAt | date }} 更新日/{{ updatedAt | date }}
+      <template v-if="date.publishedAt">
+        投稿日 {{ date.publishedAt }}
+      </template>
+      <template v-if="date.updatedAt"> 更新日 {{ date.updatedAt }} </template>
     </div>
     <div class="blog__body" v-html="bodyHtml"></div>
   </div>
 </template>
 
 <script>
-import { sourceFileArray } from '~/contents/blog.service';
+import { sourceFileArray } from '~/contents/blog.service.js';
 export default {
   validate({ params }) {
     return sourceFileArray.includes(`./contents/blog/${params.slug}/index.md`);
@@ -20,6 +23,14 @@ export default {
       require(`~/contents/blog/${params.slug}/index.json`),
       { params }
     );
+  },
+  computed: {
+    date() {
+      return {
+        publishedAt: this.$date(this.publishedAt),
+        updatedAt: this.$date(this.updatedAt)
+      };
+    }
   },
   head() {
     return {
