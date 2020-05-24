bb30878c5fa110c1979bd53092a07ea06e573319
Solenoid <maruisansmai@gmail.com>
2020-05-24 17:23:17 +0900

    change: toc position

--- pages/content/_slug.vue
+++ pages/content/_slug.vue
@@ -1,40 +1,22 @@
 <template>
-  <div class="flex flex-wrap-reverse">
-    <div class="w-full">
-      <article>
-        <!-- title -->
-        <h1 class="mb-4 text-4xl">{{ doc.title }}</h1>
-        <!-- main content -->
-        <nuxt-content :document="doc" />
-      </article>
-    </div>
-    <!-- table of content -->
-    <div class="w-full py-4 toc">
-      <h3>目次</h3>
-      <nav>
-        <ul>
-          <li
-            v-for="toc in doc.toc"
-            :key="toc.id"
-            class="text-gray-600"
-            :class="{ 'border-b': toc.depth === 2 }"
-          >
-            <a
-              :href="`#${toc.id}`"
-              class="block text-sm"
-              :class="{ 'ml-2': toc.depth === 3 }"
-            >
-              {{ toc.text }}
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+  <div class="w-full">
+    <article>
+      <!-- title -->
+      <h1 class="mb-4 text-4xl">{{ doc.title }}</h1>
+      <!-- table of content -->
+      <article-toc :toc="doc.toc" />
+      <!-- main content -->
+      <nuxt-content :document="doc" />
+    </article>
   </div>
 </template>
 
 <script>
+import ArticleToc from '@/components/ArticleToc.vue';
 export default {
+  components: {
+    ArticleToc
+  },
   async asyncData({ $content, params, error }) {
     const slug = params.slug;
 
